5e532ce5174b803c3f627377c5636bc0c4efd106
unknown <hiloloarena@gmail.com>
2019-07-31 15:25:39 +0100

    gooogleee im stiiil stupid

--- public/aaa7.php
+++ public/aaa7.php
@@ -1,49 +1,10 @@
-
-<?php
-$servername = "79.170.40.239";
-$username = "cl43-immo-rer";
-$password = "ahmed87";
-$dbz = "cl43-immo-rer";
-
-
-// Create connection
-$conn = new mysqli($servername, $username, $password,$dbz);
-
-
-$sql = "SELECT * FROM location ";
-$res = mysqli_query($conn,$sql);
+<?php 
 
 
 
-$xml = new XMLWriter();
-
-$xml->openURI("php://output");
-$xml->startDocument();
-$xml->setIndent(true);
-
-$xml->startElement('countries');
-
-while ($row = mysqli_fetch_assoc($conn,$res)) {
-  $xml->startElement("country");
-
-  $xml->writeAttribute('udid', $row['id']);
-  $xml->writeRaw($row['id']);
-
-  $xml->endElement();
-}
-
-$xml->endElement();
-
-header('Content-type: text/xml');
-$xml->flush();
+$f = new NumberFormatter("fr", NumberFormatter::SPELLOUT);
+echo $f->format(98);
 
 
 
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-
-$conn->close();
 ?>
